46326940056edfc5bbc9c638b3f66c34769deedf
Philippe Heim <Heim@ProjectJARVIS.de>
2019-12-23 10:04:00 +0100

    finish to string methods

--- src/lib/TSL/ToString.hs
+++ src/lib/TSL/ToString.hs
@@ -14,29 +14,58 @@ module TSL.ToString
   , specToString
   ) where
 
-import TSL.Specification
-  ( Specification(..)
-  , TSLSpecification(..)
-  , tslSpecToSpec
+import TSL.Specification (Specification(..), TSLSpecification(..))
+
+import TSL.Logic
+  ( Formula(..)
+  , FunctionTerm(..)
+  , PredicateTerm(..)
+  , SignalTerm(..)
   )
 
-import TSL.Logic (Formula(..))
-
-import TSL.SymbolTable (SymbolTable)
+import TSL.SymbolTable (SymbolTable, stName)
 
 -----------------------------------------------------------------------------
 -----------------------------------------------------------------------------
 insertInside :: [a] -> a -> [a]
-insertInside [] a = []
-insertInside [x] a = [a]
+insertInside [] _ = []
+insertInside [x] _ = [x]
 insertInside (x:xr) a = x : a : insertInside xr a
 
+signalTermToString :: SymbolTable -> SignalTerm Int -> String
+signalTermToString sym =
+  \case
+    Signal a -> stName sym a
+    FunctionTerm ft -> functionTermToString sym ft
+    PredicateTerm pt -> predicateTermToString sym pt
+
+functionTermToString :: SymbolTable -> FunctionTerm Int -> String
+functionTermToString sym =
+  \case
+    FunctionSymbol a -> stName sym a
+    FApplied ft st ->
+      "(" ++
+      functionTermToString sym ft ++ " " ++ signalTermToString sym st ++ ")"
+
+predicateTermToString :: SymbolTable -> PredicateTerm Int -> String
+predicateTermToString sym =
+  \case
+    BooleanTrue -> "true"
+    BooleanFalse -> "false"
+    BooleanInput a -> stName sym a
+    PredicateSymbol a -> stName sym a
+    PApplied pt st ->
+      "(" ++
+      predicateTermToString sym pt ++ " " ++ signalTermToString sym st ++ ")"
+
+formulaToString :: SymbolTable -> (Formula Int) -> String
 formulaToString sym =
   \case
     TTrue -> "true"
     FFalse -> "false"
-    Check pt -> "TODO"
-    Update c st -> "TODO"
+    Check pt -> predicateTermToString sym pt
+    Update c st ->
+      "[" ++ stName sym c ++ " <- " ++ signalTermToString sym st ++ "]"
     Not f -> "(! " ++ formulaToString sym f ++ ")"
     Implies f g ->
       "(" ++ formulaToString sym f ++ " -> " ++ formulaToString sym g ++ ")"
@@ -68,4 +97,4 @@ tslSpecToString TSLSpecification {..} =
   where
     help :: [Formula Int] -> String
     help xs =
-      concatMap (\f -> "  " ++ formulaToString symboltable f ++ ";\n") xs
+      concatMap (\f -> "  " ++ formulaToString tslSymboltable f ++ ";\n") xs
