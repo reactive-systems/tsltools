reactive-systems/tsltools

commit f10e46a95923775f7b71e1295279d910927f0017

Past Support

src/lib/TSL/Logic.hs
@@ -190,50 +190,65 @@ data Formula a =
   | And [Formula a]
   | Or [Formula a]
   | Next (Formula a)
+  | Previous (Formula a)
   | Globally (Formula a)
   | Finally (Formula a)
+  | Historically (Formula a)
+  | Once (Formula a)
   | Until (Formula a) (Formula a)
   | Release (Formula a) (Formula a)
   | Weak (Formula a) (Formula a)
+  | Since (Formula a) (Formula a)
+  | Triggered (Formula a) (Formula a)
   deriving (Eq, Ord, Show)
 
 -----------------------------------------------------------------------------
 
 instance Functor Formula where
   fmap f = \case
-    TTrue       -> TTrue
-    FFalse      -> FFalse
-    Check t     -> Check $ fmap f t
-    Update s t  -> Update (f s) $ fmap f t
-    Not x       -> Not $ fmap f x
-    Implies x y -> Implies (fmap f x) $ fmap f y
-    Equiv x y   -> Equiv (fmap f x) $ fmap f y
-    And xs      -> And $ fmap (fmap f) xs
-    Or xs       -> Or $ fmap (fmap f) xs
-    Next x      -> Next $ fmap f x
-    Globally x  -> Globally $ fmap f x
-    Finally x   -> Finally $ fmap f x
-    Until x y   -> Until (fmap f x) $ fmap f y
-    Release x y -> Release (fmap f x) $ fmap f y
-    Weak x y    -> Weak (fmap f x) $ fmap f y
+    TTrue          -> TTrue
+    FFalse         -> FFalse
+    Check t        -> Check $ fmap f t
+    Update s t     -> Update (f s) $ fmap f t
+    Not x          -> Not $ fmap f x
+    Implies x y    -> Implies (fmap f x) $ fmap f y
+    Equiv x y      -> Equiv (fmap f x) $ fmap f y
+    And xs         -> And $ fmap (fmap f) xs
+    Or xs          -> Or $ fmap (fmap f) xs
+    Next x         -> Next $ fmap f x
+    Previous x     -> Previous $ fmap f x
+    Globally x     -> Globally $ fmap f x
+    Finally x      -> Finally $ fmap f x
+    Historically x -> Historically $ fmap f x
+    Once x         -> Once $ fmap f x
+    Until x y      -> Until (fmap f x) $ fmap f y
+    Release x y    -> Release (fmap f x) $ fmap f y
+    Weak x y       -> Weak (fmap f x) $ fmap f y
+    Since x y      -> Since (fmap f x) $ fmap f y
+    Triggered x y  -> Triggered (fmap f x) $ fmap f y
 
 instance Foldable Formula where
   foldr f a = \case
-    TTrue       -> a
-    FFalse      -> a
-    Check t     -> foldr f a t
-    Update s t  -> foldr f (f s a) t
-    Not x       -> foldr f a x
-    Implies x y -> foldr f (foldr f a x) y
-    Equiv x y   -> foldr f (foldr f a x) y
-    And xs      -> foldr (flip $ foldr f) a xs
-    Or xs       -> foldr (flip $ foldr f) a xs
-    Next x      -> foldr f a x
-    Globally x  -> foldr f a x
-    Finally x   -> foldr f a x
-    Until x y   -> foldr f (foldr f a x) y
-    Release x y -> foldr f (foldr f a x) y
-    Weak x y    -> foldr f (foldr f a x) y
+    TTrue          -> a
+    FFalse         -> a
+    Check t        -> foldr f a t
+    Update s t     -> foldr f (f s a) t
+    Not x          -> foldr f a x
+    Implies x y    -> foldr f (foldr f a x) y
+    Equiv x y      -> foldr f (foldr f a x) y
+    And xs         -> foldr (flip $ foldr f) a xs
+    Or xs          -> foldr (flip $ foldr f) a xs
+    Next x         -> foldr f a x
+    Previous x     -> foldr f a x
+    Globally x     -> foldr f a x
+    Finally x      -> foldr f a x
+    Historically x -> foldr f a x
+    Once x         -> foldr f a x
+    Until x y      -> foldr f (foldr f a x) y
+    Release x y    -> foldr f (foldr f a x) y
+    Weak x y       -> foldr f (foldr f a x) y
+    Since x y      -> foldr f (foldr f a x) y
+    Triggered x y  -> foldr f (foldr f a x) y
 
 -----------------------------------------------------------------------------
 
@@ -246,21 +261,26 @@ tslSize = size' 0
 
   where
     size' a = \case
-      TTrue       -> a + 1
-      FFalse      -> a + 1
-      Check {}    -> a + 1
-      Update {}   -> a + 1
-      Not x       -> size' (a + 1) x
-      Implies x y -> size' (size' (a + 1) x) y
-      Equiv x y   -> size' (size' (a + 1) x) y
-      And xs      -> foldl size' (a + 1) xs
-      Or xs       -> foldl size' (a + 1) xs
-      Next x      -> size' (a + 1) x
-      Globally x  -> size' (a + 1) x
-      Finally x   -> size' (a + 1) x
-      Until x y   -> size' (size' (a + 1) x) y
-      Release x y -> size' (size' (a + 1) x) y
-      Weak x y    -> size' (size' (a + 1) x) y
+      TTrue          -> a + 1
+      FFalse         -> a + 1
+      Check {}       -> a + 1
+      Update {}      -> a + 1
+      Not x          -> size' (a + 1) x
+      Implies x y    -> size' (size' (a + 1) x) y
+      Equiv x y      -> size' (size' (a + 1) x) y
+      And xs         -> foldl size' (a + 1) xs
+      Or xs          -> foldl size' (a + 1) xs
+      Next x         -> size' (a + 1) x
+      Previous x     -> size' (a + 1) x
+      Globally x     -> size' (a + 1) x
+      Finally x      -> size' (a + 1) x
+      Historically x -> size' (a + 1) x
+      Once x         -> size' (a + 1) x
+      Until x y      -> size' (size' (a + 1) x) y
+      Release x y    -> size' (size' (a + 1) x) y
+      Weak x y       -> size' (size' (a + 1) x) y
+      Since x y      -> size' (size' (a + 1) x) y
+      Triggered x y  -> size' (size' (a + 1) x) y
 
 -----------------------------------------------------------------------------
 
@@ -292,25 +312,30 @@ tlsfFormula
 tlsfFormula = pr
   where
     pr = \case
-      TTrue       -> "true"
-      FFalse      -> "false"
-      Check t     -> "p0" ++ tlsfPredicate t
-      Update s t  -> "u0" ++ escape s ++ "0" ++ tlsfSignal t
-      Not x       -> "! (" ++ pr x ++ ")"
-      Implies x y -> "(" ++ pr x ++ ") -> (" ++ pr y ++ ")"
-      Equiv x y   -> "(" ++ pr x ++ ") <-> (" ++ pr y ++ ")"
-      And []      -> "true"
-      And [x]     -> pr x
-      And (x:xr)  -> foldl (\a y -> "(" ++ a ++ ") && (" ++ pr y ++ ")") (pr x) xr
-      Or []       -> "false"
-      Or [x]      -> pr x
-      Or (x:xr)   -> foldl (\a y -> "(" ++ a ++ ") || (" ++ pr y ++ ")") (pr x) xr
-      Next x      -> "X (" ++ pr x ++ ")"
-      Globally x  -> "G (" ++ pr x ++ ")"
-      Finally x   -> "F (" ++ pr x ++ ")"
-      Until x y   -> "(" ++ pr x ++ ") U (" ++ pr y ++ ")"
-      Release x y -> "(" ++ pr x ++ ") R (" ++ pr y ++ ")"
-      Weak x y    -> "(" ++ pr x ++ ") W (" ++ pr y ++ ")"
+      TTrue          -> "true"
+      FFalse         -> "false"
+      Check t        -> "p0" ++ tlsfPredicate t
+      Update s t     -> "u0" ++ escape s ++ "0" ++ tlsfSignal t
+      Not x          -> "! (" ++ pr x ++ ")"
+      Implies x y    -> "(" ++ pr x ++ ")    -> (" ++ pr y ++ ")"
+      Equiv x y      -> "(" ++ pr x ++ ") <   -> (" ++ pr y ++ ")"
+      And []         -> "true"
+      And [x]        -> pr x
+      And (x:xr)     -> foldl (\a y -> "(" ++ a ++ ") && (" ++ pr y ++ ")") (pr x) xr
+      Or []          -> "false"
+      Or [x]         -> pr x
+      Or (x:xr)      -> foldl (\a y -> "(" ++ a ++ ") || (" ++ pr y ++ ")") (pr x) xr
+      Next x         -> "X (" ++ pr x ++ ")"
+      Previous x     -> "Y (" ++ pr x ++ ")"
+      Globally x     -> "G (" ++ pr x ++ ")"
+      Finally x      -> "F (" ++ pr x ++ ")"
+      Historically x -> "H (" ++ pr x ++ ")"
+      Once x         -> "O (" ++ pr x ++ ")"
+      Until x y      -> "(" ++ pr x ++ ") U (" ++ pr y ++ ")"
+      Release x y    -> "(" ++ pr x ++ ") R (" ++ pr y ++ ")"
+      Weak x y       -> "(" ++ pr x ++ ") W (" ++ pr y ++ ")"
+      Since x y      -> "(" ++ pr x ++ ") S (" ++ pr y ++ ")"
+      Triggered x y  -> "(" ++ pr x ++ ") T (" ++ pr y ++ ")"
 
 -----------------------------------------------------------------------------
 
@@ -360,21 +385,26 @@ tlsfPredicates
 tlsfPredicates = elems . tlsfPredicates' empty
   where
     tlsfPredicates' s = \case
-      TTrue       -> s
-      FFalse      -> s
-      Update {}   -> s
-      Check p     -> insert p s
-      Not x       -> tlsfPredicates' s x
-      Implies x y -> tlsfPredicates' (tlsfPredicates' s x) y
-      Equiv x y   -> tlsfPredicates' (tlsfPredicates' s x) y
-      And xs      -> foldl tlsfPredicates' s xs
-      Or xs       -> foldl tlsfPredicates' s xs
-      Next x      -> tlsfPredicates' s x
-      Globally x  -> tlsfPredicates' s x
-      Finally x   -> tlsfPredicates' s x
-      Until x y   -> tlsfPredicates' (tlsfPredicates' s x) y
-      Release x y -> tlsfPredicates' (tlsfPredicates' s x) y
-      Weak x y    -> tlsfPredicates' (tlsfPredicates' s x) y
+      TTrue          -> s
+      FFalse         -> s
+      Update {}      -> s
+      Check p        -> insert p s
+      Not x          -> tlsfPredicates' s x
+      Implies x y    -> tlsfPredicates' (tlsfPredicates' s x) y
+      Equiv x y      -> tlsfPredicates' (tlsfPredicates' s x) y
+      And xs         -> foldl tlsfPredicates' s xs
+      Or xs          -> foldl tlsfPredicates' s xs
+      Next x         -> tlsfPredicates' s x
+      Previous x     -> tlsfPredicates' s x
+      Globally x     -> tlsfPredicates' s x
+      Finally x      -> tlsfPredicates' s x
+      Historically x -> tlsfPredicates' s x
+      Once x         -> tlsfPredicates' s x
+      Until x y      -> tlsfPredicates' (tlsfPredicates' s x) y
+      Release x y    -> tlsfPredicates' (tlsfPredicates' s x) y
+      Weak x y       -> tlsfPredicates' (tlsfPredicates' s x) y
+      Since x y      -> tlsfPredicates' (tlsfPredicates' s x) y
+      Triggered x y  -> tlsfPredicates' (tlsfPredicates' s x) y
 
 -----------------------------------------------------------------------------
 
@@ -387,21 +417,26 @@ tlsfUpdates
 tlsfUpdates = elems . tlsfUpdates' empty
   where
     tlsfUpdates' s = \case
-      TTrue       -> s
-      FFalse      -> s
-      Check {}    -> s
-      Update x t  -> insert (x,t) s
-      Not x       -> tlsfUpdates' s x
-      Implies x y -> tlsfUpdates' (tlsfUpdates' s x) y
-      Equiv x y   -> tlsfUpdates' (tlsfUpdates' s x) y
-      And xs      -> foldl tlsfUpdates' s xs
-      Or xs       -> foldl tlsfUpdates' s xs
-      Next x      -> tlsfUpdates' s x
-      Globally x  -> tlsfUpdates' s x
-      Finally x   -> tlsfUpdates' s x
-      Until x y   -> tlsfUpdates' (tlsfUpdates' s x) y
-      Release x y -> tlsfUpdates' (tlsfUpdates' s x) y
-      Weak x y    -> tlsfUpdates' (tlsfUpdates' s x) y
+      TTrue          -> s
+      FFalse         -> s
+      Check {}       -> s
+      Update x t     -> insert (x,t) s
+      Not x          -> tlsfUpdates' s x
+      Implies x y    -> tlsfUpdates' (tlsfUpdates' s x) y
+      Equiv x y      -> tlsfUpdates' (tlsfUpdates' s x) y
+      And xs         -> foldl tlsfUpdates' s xs
+      Or xs          -> foldl tlsfUpdates' s xs
+      Next x         -> tlsfUpdates' s x
+      Previous x     -> tlsfUpdates' s x
+      Globally x     -> tlsfUpdates' s x
+      Finally x      -> tlsfUpdates' s x
+      Historically x -> tlsfUpdates' s x
+      Once x         -> tlsfUpdates' s x
+      Until x y      -> tlsfUpdates' (tlsfUpdates' s x) y
+      Release x y    -> tlsfUpdates' (tlsfUpdates' s x) y
+      Weak x y       -> tlsfUpdates' (tlsfUpdates' s x) y
+      Since x y      -> tlsfUpdates' (tlsfUpdates' s x) y
+      Triggered x y  -> tlsfUpdates' (tlsfUpdates' s x) y
 
 -----------------------------------------------------------------------------
 

src/lib/TSL/Eval.hs
@@ -140,57 +140,69 @@ evalExpression
 
 evalExpression s a e =
   errPos (srcPos e) <$> case expr e of
-    BaseWild         -> return VWildcard
-    BaseTrue         -> return $ VBoolean True
-    BaseFalse        -> return $ VBoolean False
-    BaseOtherwise    -> return $ VBoolean True
-    BaseCon c        -> return $ VInt c
-    SetExplicit xs   -> VSet . fromList <$> mapM evalE xs
-    NumSMin x        -> vMinS <$> evalE x
-    NumSMax x        -> vMaxS <$> evalE x
-    NumSSize x       -> vSize <$> evalE x
-    BlnNot x         -> vNot <$> evalE x
-    TslNext x        -> vNext <$> evalE x
-    TslGlobally x    -> vGlobally <$> evalE x
-    TslFinally x     -> vFinally <$> evalE x
-    NumPlus x y      -> liftM2 vPlus (evalE x) $ evalE y
-    NumMinus x y     -> liftM2 vMinus (evalE x) $ evalE y
-    NumMul x y       -> liftM2 vMul (evalE x) $ evalE y
-    NumDiv x y       -> liftM2 vDiv (evalE x) $ evalE y
-    NumMod x y       -> liftM2 vMod (evalE x) $ evalE y
-    SetCup x y       -> liftM2 vCup (evalE x) $ evalE y
-    SetCap x y       -> liftM2 vCap (evalE x) $ evalE y
-    SetMinus x y     -> liftM2 vDif (evalE x) $ evalE y
-    BlnEQ x y        -> liftM2 vEQ (evalE x) $ evalE y
-    BlnNEQ x y       -> liftM2 vNEQ (evalE x) $ evalE y
-    BlnGE x y        -> liftM2 vGE (evalE x) $ evalE y
-    BlnGEQ x y       -> liftM2 vGEQ (evalE x) $ evalE y
-    BlnLE x y        -> liftM2 vLE (evalE x) $ evalE y
-    BlnLEQ x y       -> liftM2 vLEQ (evalE x) $ evalE y
-    BlnElem x y      -> liftM2 vElem (evalE x) $ evalE y
-    BlnAnd x y       -> liftM2 vAnd (evalE x) $ evalE y
-    BlnOr x y        -> liftM2 vOr (evalE x) $ evalE y
-    BlnImpl x y      -> liftM2 vImpl (evalE x) $ evalE y
-    BlnEquiv x y     -> liftM2 vEquiv (evalE x) $ evalE y
-    TslUntil x y     -> liftM2 vUntil (evalE x) $ evalE y
-    TslWeak x y      -> liftM2 vWeak (evalE x) $ evalE y
-    TslAsSoonAs x y  -> liftM2 vAsSoonAs (evalE x) $ evalE y
-    TslRelease x y   -> liftM2 vRelease (evalE x) $ evalE y
-    SetRange x y z   -> liftM3 vRange (evalE x) (evalE y) $ evalE z
-    NumRPlus xs x    -> evalRangeExpr vPlus x xs
-    NumRMul xs x     -> evalRangeExpr vMul x xs
-    SetRCup xs x     -> evalRangeExpr vCup x xs
-    SetRCap xs x     -> evalRangeExpr vCap x xs
-    BlnROr xs x      -> evalRangeExpr vOr x xs
-    BlnRAnd xs x     -> evalRangeExpr vAnd x xs
-    TslRNext x y     -> liftM2 vNextN (evalE x) $ evalE y
-    TslRGlobally x y -> case expr x of
+    BaseWild             -> return VWildcard
+    BaseTrue             -> return $ VBoolean True
+    BaseFalse            -> return $ VBoolean False
+    BaseOtherwise        -> return $ VBoolean True
+    BaseCon c            -> return $ VInt c
+    SetExplicit xs       -> VSet . fromList <$> mapM evalE xs
+    NumSMin x            -> vMinS <$> evalE x
+    NumSMax x            -> vMaxS <$> evalE x
+    NumSSize x           -> vSize <$> evalE x
+    BlnNot x             -> vNot <$> evalE x
+    TslNext x            -> vNext <$> evalE x
+    TslPrevious x        -> vPrevious <$> evalE x
+    TslGlobally x        -> vGlobally <$> evalE x
+    TslFinally x         -> vFinally <$> evalE x
+    TslHistorically x    -> vHistorically <$> evalE x
+    TslOnce x            -> vOnce <$> evalE x
+    NumPlus x y          -> liftM2 vPlus (evalE x) $ evalE y
+    NumMinus x y         -> liftM2 vMinus (evalE x) $ evalE y
+    NumMul x y           -> liftM2 vMul (evalE x) $ evalE y
+    NumDiv x y           -> liftM2 vDiv (evalE x) $ evalE y
+    NumMod x y           -> liftM2 vMod (evalE x) $ evalE y
+    SetCup x y           -> liftM2 vCup (evalE x) $ evalE y
+    SetCap x y           -> liftM2 vCap (evalE x) $ evalE y
+    SetMinus x y         -> liftM2 vDif (evalE x) $ evalE y
+    BlnEQ x y            -> liftM2 vEQ (evalE x) $ evalE y
+    BlnNEQ x y           -> liftM2 vNEQ (evalE x) $ evalE y
+    BlnGE x y            -> liftM2 vGE (evalE x) $ evalE y
+    BlnGEQ x y           -> liftM2 vGEQ (evalE x) $ evalE y
+    BlnLE x y            -> liftM2 vLE (evalE x) $ evalE y
+    BlnLEQ x y           -> liftM2 vLEQ (evalE x) $ evalE y
+    BlnElem x y          -> liftM2 vElem (evalE x) $ evalE y
+    BlnAnd x y           -> liftM2 vAnd (evalE x) $ evalE y
+    BlnOr x y            -> liftM2 vOr (evalE x) $ evalE y
+    BlnImpl x y          -> liftM2 vImpl (evalE x) $ evalE y
+    BlnEquiv x y         -> liftM2 vEquiv (evalE x) $ evalE y
+    TslUntil x y         -> liftM2 vUntil (evalE x) $ evalE y
+    TslWeak x y          -> liftM2 vWeak (evalE x) $ evalE y
+    TslAsSoonAs x y      -> liftM2 vAsSoonAs (evalE x) $ evalE y
+    TslRelease x y       -> liftM2 vRelease (evalE x) $ evalE y
+    TslSince x y         -> liftM2 vSince (evalE x) $ evalE y
+    TslTriggered x y     -> liftM2 vTriggered (evalE x) $ evalE y
+    SetRange x y z       -> liftM3 vRange (evalE x) (evalE y) $ evalE z
+    NumRPlus xs x        -> evalRangeExpr vPlus x xs
+    NumRMul xs x         -> evalRangeExpr vMul x xs
+    SetRCup xs x         -> evalRangeExpr vCup x xs
+    SetRCap xs x         -> evalRangeExpr vCap x xs
+    BlnROr xs x          -> evalRangeExpr vOr x xs
+    BlnRAnd xs x         -> evalRangeExpr vAnd x xs
+    TslRNext x y         -> liftM2 vNextN (evalE x) $ evalE y
+    TslRPrevious x y     -> liftM2 vPreviousN (evalE x) $ evalE y
+    TslRGlobally x y     -> case expr x of
       Colon n m -> liftM3 vGloballyR (evalE n) (evalE m) $ evalE y
       _         -> assert False undefined
-    TslRFinally x y  -> case expr x of
+    TslRFinally x y      -> case expr x of
       Colon n m -> liftM3 vFinallyR (evalE n) (evalE m) $ evalE y
       _         -> assert False undefined
-    BaseUpd x i      ->
+    TslRHistorically x y -> case expr x of
+      Colon n m -> liftM3 vHistoricallyR (evalE n) (evalE m) $ evalE y
+      _         -> assert False undefined
+    TslROnce x y         -> case expr x of
+      Colon n m -> liftM3 vOnceR (evalE n) (evalE m) $ evalE y
+      _         -> assert False undefined
+    BaseUpd x i          ->
       evalE x >>= \case
         VSTerm x'      -> return $ VTSL $ Update i x'
         VFTerm x'      -> return $ VTSL $ Update i $ FunctionTerm x'
@@ -198,11 +210,11 @@ evalExpression s a e =
         VBoolean True  -> return $ VTSL $ Update i $ PredicateTerm BooleanTrue
         VBoolean False -> return $ VTSL $ Update i $ PredicateTerm BooleanFalse
         _              -> assert False undefined
-    BaseConFn i      -> case stType s i of
+    BaseConFn i          -> case stType s i of
       TSignal (TPoly _) -> return $ VFTerm $ FunctionSymbol i
       TSignal TBoolean  -> return $ VPTerm $ PredicateSymbol i
       _                 -> assert False undefined
-    BaseId i         -> case stKind s i of
+    BaseId i             -> case stKind s i of
       Input     -> case stType s i of
         TSignal TBoolean -> return $ VPTerm $ BooleanInput i
         _                -> return $ VSTerm $ Signal i
@@ -220,9 +232,9 @@ evalExpression s a e =
             GuardedBinding xs -> vFirst xs
             _                 -> assert False undefined
         v      -> return v
-    BaseFn {}        -> evalFnApplication [] e
-    Pattern {}       -> assert False undefined
-    Colon {}         -> assert False undefined
+    BaseFn {}            -> evalFnApplication [] e
+    Pattern {}           -> assert False undefined
+    Colon {}             -> assert False undefined
 
   where
     evalE = evalExpression s a
@@ -609,6 +621,34 @@ vNextN _        _                = assert False undefined
 
 -----------------------------------------------------------------------------
 
+vPrevious
+  :: Value -> Value
+
+vPrevious (VBoolean False) = VBoolean False
+vPrevious (VPTerm x)       = VTSL $ Previous $ Check x
+vPrevious (VTSL x)         = VTSL $ Previous x
+vPrevious _                = assert False undefined
+
+-----------------------------------------------------------------------------
+
+vPreviousN
+  :: Value -> Value -> Value
+
+vPreviousN _        (VBoolean False) = VBoolean False
+vPreviousN (VInt n) (VTSL x)
+  | n < 0                            = VError undefined $
+                                     "Negative next-operator chain [" ++
+                                     show n ++ "]"
+  | otherwise                        = VTSL $ (!! n) $ iterate Previous x
+vPreviousN (VInt n) (VPTerm x)
+  | n < 0                            = VError undefined $
+                                     "Negative next-operator chain: [" ++
+                                     show n ++ "]"
+  | otherwise                        = VTSL $ (!! n) $ iterate Previous $ Check x
+vPreviousN _        _                = assert False undefined
+
+-----------------------------------------------------------------------------
+
 vGlobally
   :: Value -> Value
 
@@ -675,6 +715,74 @@ vFinallyR (VInt n) (VInt m) (VPTerm x)
   | otherwise                                =
       VError undefined $ "Invalid range: [" ++ show n ++ ":" ++ show m ++ "]"
 vFinallyR _        _        _                = assert False undefined
+
+-----------------------------------------------------------------------------
+
+vHistorically
+  :: Value -> Value
+
+vHistorically (VBoolean True)  = VBoolean True
+vHistorically (VBoolean False) = VBoolean False
+vHistorically (VPTerm x)       = VTSL $ Historically $ Check x
+vHistorically (VTSL x)         = VTSL $ Historically x
+vHistorically _                = assert False undefined
+
+-----------------------------------------------------------------------------
+
+vHistoricallyR
+  :: Value -> Value -> Value -> Value
+
+vHistoricallyR _        _        (VBoolean True)  = VBoolean True
+vHistoricallyR _        _        (VBoolean False) = VBoolean False
+vHistoricallyR (VInt n) (VInt m) (VTSL x)
+  | n > m                                    = VBoolean False
+  | n >= 0 && m >= 0                            =
+      VTSL $ (!! n) $ iterate Previous $
+        foldl (\a _ -> Or [x, Previous a]) x [0, 1 .. m - n - 1]
+  | otherwise                                =
+      VError undefined $ "Invalid range: [" ++ show n ++ ":" ++ show m ++ "]"
+vHistoricallyR (VInt n) (VInt m) (VPTerm x)
+  | n > m                                    = VBoolean False
+  | n >= 0 && m >= 0                            =
+      VTSL $ (!! n) $ iterate Previous $
+        foldl (\a _ -> Or [Check x, Previous a]) (Check x) [0, 1 .. m - n - 1]
+  | otherwise                                =
+      VError undefined $ "Invalid range: [" ++ show n ++ ":" ++ show m ++ "]"
+vHistoricallyR _        _        _                = assert False undefined
+
+-----------------------------------------------------------------------------
+
+vOnce
+  :: Value -> Value
+
+vOnce (VBoolean True)  = VBoolean True
+vOnce (VBoolean False) = VBoolean False
+vOnce (VPTerm x)       = VTSL $ Once $ Check x
+vOnce (VTSL x)         = VTSL $ Once x
+vOnce _                = assert False undefined
+
+-----------------------------------------------------------------------------
+
+vOnceR
+  :: Value -> Value -> Value -> Value
+
+vOnceR _        _        (VBoolean True)  = VBoolean True
+vOnceR _        _        (VBoolean False) = VBoolean False
+vOnceR (VInt n) (VInt m) (VTSL x)
+  | n > m                                    = VBoolean False
+  | n >= 0 && m >= 0                            =
+      VTSL $ (!! n) $ iterate Previous $
+        foldl (\a _ -> Or [x, Previous a]) x [0, 1 .. m - n - 1]
+  | otherwise                                =
+      VError undefined $ "Invalid range: [" ++ show n ++ ":" ++ show m ++ "]"
+vOnceR (VInt n) (VInt m) (VPTerm x)
+  | n > m                                    = VBoolean False
+  | n >= 0 && m >= 0                            =
+      VTSL $ (!! n) $ iterate Previous $
+        foldl (\a _ -> Or [Check x, Previous a]) (Check x) [0, 1 .. m - n - 1]
+  | otherwise                                =
+      VError undefined $ "Invalid range: [" ++ show n ++ ":" ++ show m ++ "]"
+vOnceR _        _        _                = assert False undefined
 
 -----------------------------------------------------------------------------
 
@@ -751,5 +859,40 @@ vAsSoonAs (VTSL x)         (VPTerm y)       = VTSL $ Weak (Not $ Check y) $ And
 vAsSoonAs (VPTerm x)       (VPTerm y)       = VTSL $ Weak (Not $ Check y) $
                                                 And [Check x, Check y]
 vAsSoonAs _                _                = assert False undefined
+
+-----------------------------------------------------------------------------
+
+vSince
+  :: Value -> Value -> Value
+
+vSince _                (VBoolean True)  = VBoolean True
+vSince _                (VBoolean False) = VBoolean False
+vSince (VBoolean True)  (VPTerm y)       = VTSL $ Once $ Check y
+vSince (VBoolean True)  (VTSL y)         = VTSL $ Once y
+vSince (VBoolean False) (VPTerm y)       = VTSL $ Check y
+vSince (VBoolean False) (VTSL y)         = VTSL y
+vSince (VTSL x)         (VTSL y)         = VTSL $ Since x y
+vSince (VPTerm x)       (VTSL y)         = VTSL $ Since (Check x) y
+vSince (VTSL x)         (VPTerm y)       = VTSL $ Since x $ Check y
+vSince (VPTerm x)       (VPTerm y)       = VTSL $ Since (Check x) $ Check y
+vSince _                _                = assert False undefined
+
+-----------------------------------------------------------------------------
+
+vTriggered
+  :: Value -> Value -> Value
+
+vTriggered _                (VBoolean False) = VBoolean False
+vTriggered _                (VBoolean True)  = VBoolean True
+vTriggered (VBoolean True)  (VPTerm y)       = VTSL $ Check y
+vTriggered (VBoolean True)  (VTSL y)         = VTSL y
+vTriggered (VBoolean False) (VPTerm y)       = VTSL $ Historically $ Check y
+vTriggered (VBoolean False) (VTSL y)         = VTSL $ Historically y
+vTriggered (VTSL x)         (VTSL y)         = VTSL $ Triggered x y
+vTriggered (VPTerm x)       (VTSL y)         = VTSL $ Triggered (Check x) y
+vTriggered (VTSL x)         (VPTerm y)       = VTSL $ Triggered x $ Check y
+vTriggered (VPTerm x)       (VPTerm y)       = VTSL $ Triggered (Check x) $
+                                               Check y
+vTriggered _                _                = assert False undefined
 
 -----------------------------------------------------------------------------

commit 8eef975e7b6d0df084e30fd302c3bfbcb4789d99

fix toTSLF printing bug introduced in f10e46a9

src/lib/TSL/Logic.hs
@@ -317,8 +317,8 @@ tlsfFormula = pr
       Check t        -> "p0" ++ tlsfPredicate t
       Update s t     -> "u0" ++ escape s ++ "0" ++ tlsfSignal t
       Not x          -> "! (" ++ pr x ++ ")"
-      Implies x y    -> "(" ++ pr x ++ ")    -> (" ++ pr y ++ ")"
-      Equiv x y      -> "(" ++ pr x ++ ") <   -> (" ++ pr y ++ ")"
+      Implies x y    -> "(" ++ pr x ++ ") -> (" ++ pr y ++ ")"
+      Equiv x y      -> "(" ++ pr x ++ ") <-> (" ++ pr y ++ ")"
       And []         -> "true"
       And [x]        -> pr x
       And (x:xr)     -> foldl (\a y -> "(" ++ a ++ ") && (" ++ pr y ++ ")") (pr x) xr