8693895d465859755dece3fc71f85e8086510bd9
Philippe Heim <philippe.heim@cispa.de>
2020-12-11 16:31:25 +0100

    include core-generation libraries

---
+++ src/lib/CoreGeneration/MinimalAssumptionCores.hs
@@ -0,0 +1,260 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : MinimalAssumptionCores
+-- Description : TSL minimal-assumptions finder 
+-- Maintainer  : Philippe Heim (Heim@ProjectJARVIS.de)
+--
+-- Provides method to search for a minimal amount of assumptions 
+-- needed to realize a specification.
+--
+-----------------------------------------------------------------------------
+{-# LANGUAGE LambdaCase #-}
+
+-----------------------------------------------------------------------------
+module CoreGeneration.MinimalAssumptionCores
+  ( generateMinimalAssumptions
+  , treeBasedMinimalAssumptions
+  ) where
+
+-----------------------------------------------------------------------------
+import Data.List as List (splitAt)
+
+import CoreGeneration.CoreUtilities
+  ( Context(..)
+  , logHigh
+  , logNormal
+  , optimizeSpec
+  , sortedPowerSet
+  )
+
+import Data.Set as Set (Set, difference, empty, fromList, member, toList, union)
+
+import TSL (Formula, Formula(..), Specification(..), SymbolTable, split, toTSL)
+
+import CoreGeneration.FindFirstConcurrent (incParallelFirst)
+
+-----------------------------------------------------------------------------
+-- | 'generateMinimalAssumptions' searches for a 'Specification' a minimal 
+-- sub-specification, w.r.t. the assuptions, that is realizable (a so-called
+-- assumption core) if it exists. Since this is done using synthesis queries
+-- a 'Context' is needed .
+generateMinimalAssumptions ::
+     Context -> Specification -> IO (Maybe Specification)
+generateMinimalAssumptions context tsl =
+  let assmpt = assumptions tsl
+      indicesList = sortedPowerSet $ length assmpt
+      potQueries =
+        fmap
+          (\indices ->
+             ( (tsl
+                  { assumptions =
+                      fmap snd $
+                      Prelude.filter (\(a, _) -> member a indices) $
+                      zip [0 ..] assmpt
+                  })
+             , length indices))
+          indicesList
+      queries = fst <$> Prelude.filter (uncurry splitedAssumptions) potQueries
+   in incParallelFirst (threadPoolSize context) (fmap test queries)
+  where
+    test :: Specification -> IO (Maybe Specification)
+    test spec = do
+      logNormal context $
+        "Test query with " ++ show (length (assumptions spec)) ++ " assumptions"
+      logHigh context $ "Query:\n" ++ toTSL (optimizeSpec spec) ++ "\n"
+      rel <- tslSpecRealizable context (optimizeSpec spec)
+      if rel
+        then return $ Just spec
+        else return Nothing
+    -- Not already seen
+    splitedAssumptions spec num =
+      case split spec of
+        [s] -> length (assumptions s) == num
+        _ -> False
+
+-----------------------------------------------------------------------------
+-- | 'AssumptionTree' is a binary tree which repesents which assumptions are
+-- needed for some combination of . The guarantees are in the leafs of the 
+-- three. The Nodes are annoted with assumptions repersenting that its
+-- children need this assumtpions.
+data AssumptionTree
+  -- | A 'Leaf' of an 'AssumptionTree' holds a guarantee and a set
+  -- of assumptions (which are of type 'Formula' 'Int') each.
+  = Leaf (Set (Formula Int)) (Formula Int)
+  -- | A (inner) 'Node' of an 'AssumptionTree' holds the set of
+  -- assumptions that allows the conjunction of both sub-trees
+  -- to by synthesized.
+  | Node (Set (Formula Int)) AssumptionTree AssumptionTree
+
+-----------------------------------------------------------------------------
+-- | 'liftAssumption' aims to reduce the amount of assumptions in an 
+-- 'AssumptionTree' without changing the realizability by lifting the common
+-- assumptions of two children to their common parent.
+liftAssumptions :: AssumptionTree -> AssumptionTree
+liftAssumptions =
+  \case
+    Node assmpt t1 t2 ->
+      let t1p = liftAssumptions t1
+          t2p = liftAssumptions t2
+          commonDirectSubAssumptions =
+            union (directSubAssumptions t1p) (directSubAssumptions t2p)
+       in Node
+            (assmpt `union` commonDirectSubAssumptions)
+            (removeDirectSubAssumption commonDirectSubAssumptions t1p)
+            (removeDirectSubAssumption commonDirectSubAssumptions t2p)
+    leaf -> leaf
+  where
+    directSubAssumptions =
+      \case
+        Leaf asmpt _ -> asmpt
+        Node asmpt _ _ -> asmpt
+    removeDirectSubAssumption rmAsmpt =
+      \case
+        Leaf asmpt g -> Leaf (difference asmpt rmAsmpt) g
+        Node asmpt t1 t2 -> Node (difference asmpt rmAsmpt) t1 t2
+
+-----------------------------------------------------------------------------
+-- | 'liftAssumption' aims to reduce the amount of assumptions in an 
+-- 'AssumptionTree' without changing the realizability by removing (redundant)
+-- assumptions that are already required in the parent.
+cleanAssumptions :: AssumptionTree -> AssumptionTree
+cleanAssumptions = help empty
+  where
+    help :: Set (Formula Int) -> AssumptionTree -> AssumptionTree
+    help known =
+      \case
+        Leaf asmpt g -> Leaf (difference asmpt known) g
+        Node asmpt t1 t2 ->
+          let newKnown = union asmpt known
+           in Node
+                (difference asmpt known)
+                (help newKnown t1)
+                (help newKnown t2)
+
+-----------------------------------------------------------------------------
+-- | 'reduce' removes unecessary assumptions of an 'AssumptionTree' by first
+-- lifting assumptions (using 'liftAssumpions') and then cleaning up 
+-- (using 'cleanAssumptions'). This functions should be used when reducing
+-- assumption trees over the individual optimizations
+reduce :: AssumptionTree -> AssumptionTree
+reduce = cleanAssumptions . liftAssumptions
+
+-----------------------------------------------------------------------------
+-- | 'flatten' converst an 'AssumptionTree' into a conjunction of formulas
+-- (represented as list of 'Formula' 'Int') which are equvivalent to
+-- the formula represented by the 'AssumptionTree'. Therefore, the 
+-- distributivty law of the implication is applied through the tree.
+flatten :: AssumptionTree -> [Formula Int]
+flatten =
+  \case
+    Leaf asmpt g -> [applyAssumptions asmpt g]
+    Node asmpt t1 t2 -> fmap (applyAssumptions asmpt) (flatten t1 ++ flatten t2)
+  where
+    applyAssumptions asmpt formula =
+      case toList asmpt of
+        [] -> formula
+        xs -> Or $ fmap Not xs ++ [formula]
+
+-----------------------------------------------------------------------------
+-- | 'assumptionTreeToSpec' generates a 'Specification' out of an 
+-- 'AssumtptionTree', represeting the guarantees, a list of 'Formula Int'
+-- represtenting additional assumptions and a respective 'SymbolTable'. 
+assumptionTreeToSpec ::
+     SymbolTable -> [Formula Int] -> AssumptionTree -> Specification
+assumptionTreeToSpec sym asmpt atree =
+  Specification
+    { assumptions = asmpt
+    , guarantees = flatten (reduce atree)
+    , symboltable = sym
+    }
+
+-----------------------------------------------------------------------------
+-- | 'SearchTree' reperesents a binary search tree, where the searched 
+-- objects are at the leafs
+data SearchTree a
+  = STLeaf a
+  | STNode (SearchTree a) (SearchTree a)
+
+-----------------------------------------------------------------------------
+-- | 'searchTree' generates a 'SearchTree' out of a list where the elements 
+-- of the list are at the leaf of the 'SearchTree'. Note that 'searchTree'
+-- aims for a balanced search tree.
+searchTree :: [a] -> Maybe (SearchTree a)
+searchTree =
+  \case
+    [] -> Nothing
+    [x] -> Just (STLeaf x)
+    xs ->
+      let (a, b) = splitAt (length xs `div` 2) xs
+       in case (searchTree a, searchTree b) of
+            (Nothing, Nothing) -> Nothing
+            (Just s, Nothing) -> Just s
+            (Nothing, Just s) -> Just s
+            (Just s, Just t) -> Just (STNode s t)
+
+-----------------------------------------------------------------------------
+-- | 'minimalAssumptionTree' searches for an 'AssuptionTree' with a minimal
+-- amount of assumptions provide a 'SearchTree' for the order the guarantees 
+-- should be searched through. Note that for a specific node the 'AssumptionTree'
+-- is convereted into a 'Specification' and the minimal amount of assuptions is 
+-- searched using 'generateMinimalAssumptions'
+minimalAssumptionTree ::
+     Context
+  -> [Formula Int]
+  -> SymbolTable
+  -> SearchTree (Formula Int)
+  -> IO (Maybe AssumptionTree)
+minimalAssumptionTree context asmpt sym =
+  \case
+    STLeaf g -> do
+      logNormal context "Start searching leaf"
+      potSpec <-
+        subGeneration
+          (Specification
+             {symboltable = sym, guarantees = [g], assumptions = asmpt})
+      logNormal context "Finished searching leaf"
+      case potSpec of
+        Nothing -> return Nothing
+        Just spec -> return $ Just $ Leaf (fromList $ assumptions spec) g
+    STNode t1 t2 -> do
+      a1p <- minimalAssumptionTree context asmpt sym t1
+      a2p <- minimalAssumptionTree context asmpt sym t2
+      case (a1p, a2p) of
+        (Just a1, Just a2) -> do
+          let spec1 = assumptionTreeToSpec sym [] a1
+          let spec2 = assumptionTreeToSpec sym [] a2
+          logNormal context "Start searching inner node"
+          potSpec <-
+            subGeneration
+              (Specification
+                 { symboltable = sym
+                 , guarantees = guarantees spec1 ++ guarantees spec2
+                 , assumptions = asmpt
+                 })
+          logNormal context "Finished searching inner node"
+          case potSpec of
+            Nothing -> return Nothing
+            Just spec ->
+              return $ Just $ Node (fromList $ assumptions spec) a1 a2
+        _ -> return Nothing
+  where
+    subGeneration = generateMinimalAssumptions context
+
+-----------------------------------------------------------------------------
+-- | Similar to 'generateMinimalAssumptions', 'treeBasedMinimalAssumptions' 
+-- searches for a 'Specification' a minimal sub-specification, w.r.t. the 
+-- assuptions, that is realizable (a so-called assumption core) if it exists.
+-- The main difference it that is uses a 'AssumptionTree' to perform this 
+-- search. 
+treeBasedMinimalAssumptions ::
+     Context -> Specification -> IO (Maybe Specification)
+treeBasedMinimalAssumptions context spec =
+  case searchTree (guarantees spec) of
+    Nothing -> return Nothing
+    Just st -> do
+      potATree <-
+        minimalAssumptionTree context (assumptions spec) (symboltable spec) st
+      case potATree of
+        Nothing -> return Nothing
+        Just aTree ->
+          return $ Just $ assumptionTreeToSpec (symboltable spec) [] aTree
---
+++ src/lib/CoreGeneration/UnrealizabilityCores.hs
@@ -0,0 +1,166 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : UnrealizabilityCores
+-- Description : TSL unrealizable-cores 
+-- Maintainer  : Philippe Heim (Heim@ProjectJARVIS.de)
+--
+-- Provides methods to generate unrealizability cores
+--
+-----------------------------------------------------------------------------
+{-# LANGUAGE ViewPatterns, LambdaCase, RecordWildCards #-}
+
+-----------------------------------------------------------------------------
+module CoreGeneration.UnrealizabilityCores
+  ( generateCore
+  ) where
+
+-----------------------------------------------------------------------------
+import CoreGeneration.CoreUtilities
+  ( Context(..)
+  , logHigh
+  , logNormal
+  , sortedPowerSet
+  )
+
+import Data.Set as Set
+  ( Set
+  , difference
+  , empty
+  , filter
+  , fromList
+  , isSubsetOf
+  , map
+  , member
+  , size
+  , toList
+  , union
+  , unions
+  )
+
+import TSL
+  ( Formula
+  , Formula(..)
+  , SignalTerm(..)
+  , Specification(..)
+  , outputs
+  , split
+  , toTSL
+  , updates
+  )
+
+import CoreGeneration.FindFirstConcurrent (incParallelFirst)
+
+-----------------------------------------------------------------------------
+-- | 'Query' represents some potential core and is therfore a 'Specification'
+type Query = Specification
+
+-----------------------------------------------------------------------------
+-- | 'getCores' computes a list of possible queries that can be used to find
+-- a unrelaizability core. Note that the queries are sorted in such a way 
+-- that the first unrelaizable one is a core with a minimal amount of
+-- guarantees.
+getCores :: Specification -> [Query]
+getCores tsl@Specification {guarantees = g} =
+  concatMap
+    (\indices ->
+       fmap (\spec -> spec {guarantees = addMissingUpdates (guarantees spec)}) $
+       (\specs ->
+          if length specs == 1
+            then specs
+            else []) $
+       Prelude.filter
+         (not . null . guarantees)
+         (split (tsl {guarantees = choose indices})))
+    (sortedPowerSet $ length g)
+  where
+    choose indices =
+      fmap snd $ Prelude.filter (\(a, _) -> member a indices) $ zip [0 ..] g
+    addMissingUpdates choosen =
+      let otherUpdates =
+            Or $
+            TTrue :
+            Set.toList
+              (Set.difference
+                 (getPossibleUpdates (And g) (unions $ fmap outputs choosen))
+                 (Set.map (uncurry Update) $ updates (And g)))
+       in choosen ++ [otherUpdates]
+    --
+    getPossibleUpdates :: Ord c => Formula c -> Set c -> Set (Formula c)
+    getPossibleUpdates form outps =
+      let selfUpdates = Set.map (\o -> (o, Signal o)) (outputs form)
+       in Set.map (uncurry Update) $
+          Set.filter (\(s, _) -> member s outps) $
+          union (updates form) selfUpdates
+
+-----------------------------------------------------------------------------
+-- | 'testCoreQuery' checks whether some potential unrealizable core is
+-- actually one. To make this more efficent assumption searching is used
+-- to find realizbale (non-core) specifications faster. 'testCoreQuery' is
+-- provideds a set of assumptions that is surley needed known from the 
+-- previous queries.
+testCoreQuery ::
+     Context -> Set (Formula Int) -> Specification -> IO (Maybe Specification)
+testCoreQuery context minimalAssumptions tsl =
+  let assmpt = assumptions tsl
+      subQueries =
+        Prelude.filter
+          (\spec -> minimalAssumptions `isSubsetOf` fromList (assumptions spec))
+          [ tsl
+            { assumptions =
+                fmap snd $
+                Prelude.filter (\(a, _) -> member a indices) $ zip [0 ..] assmpt
+            }
+          | indices <-
+              Prelude.filter (\set -> size set >= min (length assmpt) 3) $
+              sortedPowerSet $ length assmpt
+          ]
+   in do out <- incParallelFirst (threadPoolSize context) (fmap test subQueries)
+         case out of
+           Just True -> return Nothing
+           _ -> do
+             logNormal context $
+               "Test Core with " ++
+               show (length (assumptions tsl)) ++ " assumptions"
+             rel <- tslSpecRealizable context tsl
+             if rel
+               then return Nothing
+               else return (Just tsl)
+  where
+    test :: Specification -> IO (Maybe Bool)
+    test spec = do
+      logNormal context $
+        "Test Core with " ++ show (length (assumptions spec)) ++ " assumptions"
+      rel <- tslSpecRealizable context spec
+      if rel
+        then return (Just True)
+        else return Nothing
+
+-----------------------------------------------------------------------------
+-- | 'generateCore' computes for a 'Specification' a minimal 
+-- sub-specification with a subset of guarantees that is unrealizable 
+-- (if it exists). This is called a unrealizable core. The computation is
+-- done using synthesis calls and therfore a 'Context' is needed
+generateCore :: Context -> Specification -> IO (Maybe Specification)
+generateCore context tsl = do
+  let queries = getCores tsl
+  genCore' (const empty) queries
+  where
+    genCore' ::
+         (Set (Formula Int) -> Set (Formula Int))
+      -> [Query]
+      -> IO (Maybe Specification)
+    genCore' _ [] = return Nothing
+    genCore' minimalAssumptions (q:qr) = do
+      logHigh context $ "Check core:\n" ++ toTSL q ++ "\n"
+      out <-
+        testCoreQuery context (minimalAssumptions (fromList $guarantees q)) q
+      case out of
+        Nothing ->
+          genCore'
+            (\set ->
+               minimalAssumptions set `union`
+               (if fromList (guarantees q) `isSubsetOf` set
+                  then fromList $ assumptions q
+                  else empty))
+            qr
+        core -> return core
