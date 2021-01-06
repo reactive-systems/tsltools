602ef88096a9c0b1dc2328ece1e13c409af59e13
Marvin Stenger <stenger@projectjarvis.de>
2021-01-06 13:33:14 +0100

    tsl2tlsf: rework argparsing + use tryLoadTSL

--- src/tools/tsl2tlsf/Main.hs
+++ src/tools/tsl2tlsf/Main.hs
@@ -7,55 +7,30 @@
 --
 -----------------------------------------------------------------------------
 
-{-# LANGUAGE
-
-    LambdaCase
-  , ImplicitParams
-
-  #-}
-
------------------------------------------------------------------------------
-
 module Main
   ( main
   ) where
 
 -----------------------------------------------------------------------------
 
-import PrintUtils
-  ( Color(..)
-  , ColorIntensity(..)
-  , putErr
-  , printErrLn
-  , cPutOut
-  , cPutOutLn
-  , cPutErr
-  , cPutErrLn
-  )
-
 import EncodingUtils
   ( initEncoding
   )
 
-import TSL
-  ( fromTSL
-  , toTLSF
+import ArgParseUtils
+  ( parseMaybeFilePath
   )
 
-import System.Directory
-  ( doesFileExist
+import FileUtils
+  ( tryLoadTSL
+  )
+
+import TSL
+  ( toTLSF
   )
 
 import System.FilePath
   ( takeBaseName
-  )
-
-import System.Environment
-  ( getArgs
-  )
-
-import System.Exit
-  ( exitFailure
   )
 
 -----------------------------------------------------------------------------
@@ -66,35 +41,14 @@ main
 main = do
   initEncoding
 
-  args <- getArgs -- TODO: no Config.hs needed?
-  if null args
-  then do
-    let ?specFilePath = Nothing 
-    getContents >>= fromTSL >>= \case
-      Left err -> do
-        cPutOutLn Vivid Red "invalid"
-        printErrLn err
-        exitFailure
-      Right s  ->
-        putStr $ toTLSF "stdin" s
-  else do
-    let path = head args
-    let ?specFilePath = Just path
+  input <- parseMaybeFilePath "tsl2tlsf"
 
-    exists <- doesFileExist path
+  spec <- tryLoadTSL input
 
-    if not exists then do
-      cPutErr Vivid Red "File not found: "
-      cPutErrLn Vivid White path
-      exitFailure
-    else
-      readFile path >>= fromTSL >>= \case
-        Left err -> do
-          cPutOut Vivid Red "invalid: "
-          cPutOutLn Vivid White path
-          printErrLn err
-          exitFailure
-        Right s  ->
-          putStr $ toTLSF (takeBaseName path) s
+  putStrLn $ toTLSF (
+      case input of
+        Nothing -> "STDIN"
+        Just file -> takeBaseName file
+    ) spec
 
 -----------------------------------------------------------------------------
