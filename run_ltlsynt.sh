9f84f57ee6e3355585dfbf8da4805a355a240843
Mark Santolucito <mark.santolucito@yale.edu>
2022-01-25 07:37:12 -0500

    actually generate readable pseudocode

--- run_ltlsynt.sh
+++ run_ltlsynt.sh
@@ -6,7 +6,8 @@ IN=$(syfco $1 --print-input-signals)
 OUT=$(syfco $1 --print-output-signals)
 ltlsynt --formula="$LTL" --ins="$IN" --outs="$OUT" > $filename.hoa
 sed -i 1d $filename.hoa
-./parsehoa $filename.hoa > parsed_$filename.hoa
-autfilt parsed_$filename.hoa --deterministic --dot > $filename.dot
+./hoa2code $filename.hoa --arrow > $filename.code
+./parsehoa $filename.hoa > $filename.parsed.hoa
+autfilt $filename.parsed.hoa --deterministic --dot > $filename.dot
 sed -i 's/|/|\\n/g' $filename.dot
 dot -Tpng $filename.dot -o $filename.png
--- src/tools/hoa2code/Main.hs
+++ src/tools/hoa2code/Main.hs
@@ -28,44 +28,22 @@ import Config (Configuration(..), parseArguments)
 
 import EncodingUtils (initEncoding)
 
-import FileUtils (loadCFM, writeContent)
 
-import TSL (implement)
-import TSL (decodeOutputAP) 
-import TSL (decodeInputAP)
-import TSL (tslFormula)
+import TSL ( decodeOutputAP, decodeInputAP, tslFormula )
 import qualified TSL as T (Formula(..))
-import Data.Either 
-import Data.Char 
-import Data.List 
-import Data.Tuple
+import Data.List ( isPrefixOf, isInfixOf )
+import Data.Tuple ( swap )
 
-import Hanoi 
-  ( HOA(..)
-  , parse
-  )
-import System.Environment
-import Data.Maybe
-import Data.Tuple
+import Hanoi
+    ( HOA(..), parse, AcceptanceSet, Label, State, Formula(..) )
+import Data.Maybe ( fromJust, maybeToList )
 import Data.List as List (intercalate, sortOn)
 
 import Finite (Finite, FiniteBounds, index, offset, v2t, values)
 
 import qualified Data.Map as M
-import Data.List as List (intercalate, sortOn)
-import Data.Maybe (maybeToList)
 import Data.Set as Set (Set, elems, toList)
-import Finite (Finite, FiniteBounds, index, offset, v2t, values)
-import Hanoi
-  ( AcceptanceSet
-  , AcceptanceType(..)
-  , HOA(..)
-  , HOAAcceptanceName(..)
-  , HOAProperty(..)
-  , Label
-  , State
-  )
-import Hanoi (Formula(..))
+import qualified Data.Bifunctor
 
 -----------------------------------------------------------------------------
 
@@ -92,113 +70,125 @@ printHOA = unlines . printHOALines
 -- | 'printHOA' prints a 'HOA' as a list of 'String's representing
 -- different lines
 
-stripQuotes :: String -> String
-stripQuotes =
-  init. tail
-
 printHOALines :: HOA -> [String]
 printHOALines hoa@HOA {..} =
   let ?bounds = hoa in
-  let 
-    apNamesSorted = map (\(l, idx) -> (atomicPropositionName l, idx)) $ sortOn (atomicPropositionName. fst) $ zip values [0..]
+  let
+    apNamesSorted = map (Data.Bifunctor.first atomicPropositionName) $ sortOn (atomicPropositionName. fst) $ zip values [0..]
     apNamesMap = M.fromList $ map swap apNamesSorted
 
-    printState :: FiniteBounds HOA => M.Map Int String -> State -> [String]
-    printState nameMap s =
+    printState :: FiniteBounds HOA => State -> [String]
+    printState s =
       unwords (
         ["\nif (currentState == "]
         ++
-        maybeToList (printLabel <$> stateLabel s)
-        ++
         [strInd s]
-        ++ [") :"]
         ++
-        (case stateAcceptance s of
-          Just aSets -> [brCurly $ unwords (map strInd $ elems aSets)]
-          Nothing    -> []
-        )
+        ["):", indent 1]
       )
       :
-      map (("  " ++) . printEdge) (toList $ edges s)
+      map printEdge (toList $ edges s)
 
     printEdge ::
           FiniteBounds HOA
       => ([State], Maybe Label, Maybe (Set AcceptanceSet))
       -> String
     printEdge edge =
-      let (target, label, aSets) = edge
+      let
+        (target, label, _) = edge
+        stateUpdate = "currentState = " ++ printStateConj target
       in
-      unwords . concat $
-        [ maybeToList $ printLabel <$> label
-        , ["\n  currentState = "] ++ [printStateConj target]
-        , maybeToList $ printAcceptanceSets <$> aSets
-        ]
+        printLabel (fromJust label) stateUpdate
 
-    printAcceptanceSets :: FiniteBounds HOA => Set AcceptanceSet -> String
-    printAcceptanceSets aSets =
-      brCurly $ unwords $ map strInd $ toList aSets
+    printLabel :: FiniteBounds HOA => Label -> String -> String
+    printLabel label stateUpdate = let
 
-
-    printLabel :: FiniteBounds HOA => Label -> String
-    printLabel label = "if (" ++ printFormula strInd2 label
+        splitFormulas = formulaToList label
+        termStringList = map (map (printTSLFormula strInd2)) splitFormulas :: [[String]]
+        predUpds = splitPredUpdates termStringList
+        predUpdToCode (preds, upds) = let
+            conditional =  intercalate (" &&" ++ indent 3) preds
+            body = indent 4 ++ intercalate (indent 4) (upds ++ [stateUpdate])
+          in
+            "if (" ++ conditional ++ "):" ++ body
+      in
+        concatMap (\x -> indent 2 ++ predUpdToCode x) predUpds
 
     printStateConj :: FiniteBounds HOA => [State] -> String
     printStateConj = intercalate " & " . map strInd
 
     strInd2 = strIndWithMap apNamesMap
   in
-    concatMap (printState apNamesMap) values
+    concatMap printState values
 
 -----------------------------------------------------------------------------
 -- | Different library related printing methods
+
+indent :: Int -> String
+indent n = let
+    indentLevel = "  "
+  in
+    "\n" ++ concat (replicate n indentLevel)
 
 strIndWithMap :: (Finite HOA a, FiniteBounds HOA) => M.Map Int String -> a -> String
 strIndWithMap nameMap a = translateToTSL $ nameMap M.! (index a - offset (v2t a))
 
 strInd :: (Finite HOA a, FiniteBounds HOA) => a -> String
-strInd a = (show (index a - offset (v2t a)))
-
-
+strInd a = show (index a - offset (v2t a))
 
 wrap :: String -> String -> String -> String
 wrap prefix suffix s = prefix ++ s ++ suffix
 
-
 brRound :: String -> String
 brRound = wrap "(" ")"
 
-brBox :: String -> String
-brBox = wrap "[" "]"
-
-brCurly :: String -> String
-brCurly = wrap "{" "}"
-
 translateToTSL :: String -> String
-translateToTSL t = 
+translateToTSL t =
   if isPrefixOf "p0" t
   then generateTSLString T.Check decodeInputAP t
   else generateTSLString (uncurry T.Update) decodeOutputAP t
 
 generateTSLString :: forall a b. Show a => _ -> (String -> Either a b) -> String -> String
 generateTSLString tslType decoder x =
-  either (show) (\t -> (tslFormula id $ tslType t)) $
-    (decoder) x
------------------------------------------------------------------------------
--- | 'printFormula' prints a 'Formula' as a 'String'. Note that the way
--- of printing these formulas is HOA specific and therefore not part of
--- the HOA.Formula module.
+  either show (tslFormula id. tslType) $
+    decoder x
 
-printFormula :: (a -> String) -> Formula a -> String
-printFormula showVar = \case
-  FTrue -> "t"
-  FFalse -> "f"
+
+splitPredUpdates :: [[String]] -> [([String], [String])]
+splitPredUpdates terms = let
+    sortTerm t (preds, upds) =
+      if isInfixOf "<-" t
+      then if isPrefixOf "!" t
+        then (preds, upds)
+        else (preds, t:upds)
+      else (t:preds, upds)
+  in
+    map (foldr sortTerm ([],[])) terms
+
+-- | in TSL, the formulas on the edges will always be conjunctions separated by disjunctions
+--   so we just pull the formula apart, then can sort by preds and updates 
+formulaToList :: Formula a -> [[Formula a]]
+formulaToList f =
+  map conjunctionsToList $ disjunctionsToList f
+
+conjunctionsToList :: Formula a -> [Formula a]
+conjunctionsToList f = case f of
+  FAnd fs -> concatMap conjunctionsToList fs
+  _ -> [f]
+
+disjunctionsToList :: Formula a -> [Formula a]
+disjunctionsToList f = case f of
+  FOr fs -> concatMap disjunctionsToList fs
+  _ -> [f]
+
+-- | When printing a TSL formula, we should only have the vars or negated vars
+--   if we have anything else, we did not have only conjunctions and need to revisit assumption of formulaToList
+printTSLFormula :: (a -> String) -> Formula a -> String
+printTSLFormula showVar = \case
   FVar a -> showVar a
   FNot f -> "!" ++ printSubFormula f
-  FAnd fs ->
-    intercalate " & " $ fmap printSubFormula fs
-  FOr fs ->
-    intercalate " | " $ fmap printSubFormula fs
+  _ -> error "unexpected formula structure on transition"
 
   where
-    printSubFormula = (brRound . printFormula showVar)
+    printSubFormula = brRound . printTSLFormula showVar
 
