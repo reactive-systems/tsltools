8f65e945bb4fe29c099d5179704f8097280c293b
Mark Santolucito <msantolu@barnard.edu>
2022-02-14 04:36:35 -0500

    add tslsynt code

---
+++ src/tools/tslsynth/Config.hs
@@ -0,0 +1,143 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Config
+-- Maintainer  :  Felix Klein
+--
+-- Configuration of the tool, set up via the command line arguments.
+-- TODO move to tool-utlis
+-----------------------------------------------------------------------------
+
+{-# LANGUAGE LambdaCase     #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+-----------------------------------------------------------------------------
+
+module Config
+  ( Configuration(..)
+  , parseArguments
+  ) where
+
+-----------------------------------------------------------------------------
+
+import PrintUtils (Color(..), ColorIntensity(..), cPutErr, cPutErrLn)
+
+import Data.Semigroup ((<>))
+import Options.Applicative
+
+import TSL (CodeTarget(..))
+
+import Data.Char (toLower, toUpper)
+
+import System.Exit (exitFailure)
+
+import Control.Monad (unless)
+
+import System.Directory (doesFileExist)
+
+import System.FilePath (takeBaseName)
+
+-----------------------------------------------------------------------------
+
+-- | The data type contains all flags and settings
+-- that can be adjusted to influence the behavior of the library:
+data Configuration =
+  Configuration
+  { -- | The input file containing the synthesized control flow
+    -- model. If no input file is given we read from STDIN.
+    input :: Maybe FilePath
+    -- | Output file path. If no path is given, the output is writtend
+    -- to STDOUT.
+  , output :: Maybe FilePath
+    -- | Target code type to generate.
+  , codeTarget :: CodeTarget
+    -- | The name of the generated module.
+  , moduleName :: String
+    -- | Name of the synthesized signal function that is exported by
+    -- the module.
+  , functionName :: String
+  } deriving (Eq, Ord)
+
+configParser :: Parser Configuration
+configParser = Configuration
+  <$> optional (argument str
+        (  metavar "INFILE"
+        <> help "input file (STDIN, if not set)"
+        )
+      )
+  <*> optional (option str
+        (  long "output"
+        <> short 'o'
+        <> metavar "OUTFILE"
+        <> help "output file (STDOUT, if not set)"
+        )
+      )
+  <*> ( flag' Applicative (long "applicative" <> help "generates code for 'Applicative'-FRP libraries")
+    <|> flag' Monadic     (long "monadic"     <> help "generates code for 'Monadic'-FRP libraries")
+    <|> flag' Arrow       (long "arrow"       <> help "generates code for 'Arrowized'-FRP libraries")
+    <|> flag' Clash       (long "clash"       <> help "generates code for the hardware description language 'ClaSH'")
+    <|> flag' JavaScript  (long "javascript"  <> help "generates code for Javascript")
+    <|> flag' WebAudio    (long "webaudio"    <> help "generates code for JS+WebAudio backend")
+    <|> flag' Python      (long "python"      <> help "generates code for Python")
+    )
+  <*> option str
+      (  long "module-name"
+      <> short 'm'
+      <> help "overwrites the name of the generated module; if not set, the filename of the passed input file is used; if reading from STDIN, the default 'Synth' is used"
+      <> metavar "MOD"
+      <> value ""
+      )
+  <*> option str
+      (  long "function-name"
+      <> short 'f'
+      <> help "overwrites the name of the exported function; if not set, the filename of the passed input file is used; if reading from STDIN, the default 'cfm' is used"
+      <> metavar "FUNC"
+      <> value ""
+      )
+
+configParserInfo :: ParserInfo Configuration
+configParserInfo = info (configParser <**> helper)
+  (  fullDesc
+  <> header "cfm2code - generates code from a TSL control flow model"
+  )
+
+parseArguments :: IO Configuration
+parseArguments = do
+  c@Configuration{input} <- customExecParser pprefs configParserInfo
+
+  case input of
+    Just file -> do
+      exists <- doesFileExist file
+      unless exists $ argsError "File not found" file
+    Nothing -> return ()
+
+  return
+    c { moduleName = fstUpper $ case moduleName c of
+          "" -> case input of
+            Just file -> takeBaseName file
+            Nothing   -> "Synth"
+          x  -> x
+      , functionName = fstLower $ case functionName c of
+          "" -> case input of
+            Just file -> takeBaseName file
+            Nothing   -> "cfm"
+          x  -> x
+      }
+
+  where
+    pprefs = prefs disambiguate
+
+    argsError h str = do
+      unless (null h) $
+        cPutErr Vivid Red $ h ++ ": "
+      cPutErrLn Dull White str
+      exitFailure
+
+    fstLower = \case
+      []   -> []
+      x:xr -> toLower x : xr
+
+    fstUpper = \case
+      []   -> []
+      x:xr -> toUpper x : xr
+
+-----------------------------------------------------------------------------
---
+++ src/tools/tslsynth/Main.hs
@@ -0,0 +1,118 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Main
+-- Maintainer  :  Mark Santolucito
+--
+-- Runs the full synthesis pipeline
+--
+-----------------------------------------------------------------------------
+{-# LANGUAGE NamedFieldPuns #-}
+-----------------------------------------------------------------------------
+
+module Main
+  ( main
+  ) where
+
+-----------------------------------------------------------------------------
+
+import Config (Configuration(..), parseArguments)
+import EncodingUtils (initEncoding)
+import FileUtils (loadTSL)
+ 
+import TSL (toTLSF, implementHoa)
+
+import Hanoi (parse)
+
+import Data.Maybe (fromJust)
+import Data.List (isPrefixOf)
+
+import System.FilePath.Posix (takeBaseName)
+
+import System.Process (readProcess)
+
+import qualified Syfco as S
+-----------------------------------------------------------------------------
+
+main
+  :: IO ()
+
+main = do
+  initEncoding
+
+  Configuration{input, output, codeTarget, moduleName, functionName} <- parseArguments
+  let fileBasename = takeBaseName $ fromJust input
+
+  -- tsl2tlsf
+  spec <- loadTSL input
+  let tlsfSpec = toTLSF fileBasename spec
+  
+  -- call ltlSynt
+  hoaOutput <- callLtlsynt tlsfSpec
+
+  hoaContents <- 
+    if isPrefixOf "REALIZABLE" hoaOutput
+    then do
+      return $ unlines $ tail $ lines $ hoaOutput
+    else 
+      print hoaOutput >>
+      error "unrealizable spec"
+      --error $ tslCoreGen $ fromJust input
+
+  let hoa = parse hoaContents
+  putStrLn $ either id (implementHoa codeTarget) hoa
+
+{-
+TODO figure out how the realizableCommand should work
+tslCoreGen :: String -> IO()
+tslCoreGen spec = generateCore (createContext poolSize verbosity' realizableCommand) spec
+  >>= \case
+    Nothing -> error "HOA file did not start with REALIZABLE, but the TSL spec was realizable"
+    Just core -> do
+      putStrLn "UNREALIZABLE CORE"
+      putStr $ toTSL core
+-}
+
+callLtlsynt :: String -> IO String
+callLtlsynt tlsfContents = do
+  let tlsfSpec = 
+        case S.fromTLSF tlsfContents of
+          Left err -> error $ show err
+          Right spec -> spec 
+  let  
+    ltlIns      = prInputs S.defaultCfg tlsfSpec
+    ltlOuts     = prOutputs S.defaultCfg tlsfSpec
+    ltlFormulae = prFormulae S.defaultCfg{ S.outputMode = S.Fully, S.outputFormat = S.LTLXBA } tlsfSpec
+    ltlCommandArgs = [ltlFormulae, ltlIns, ltlOuts]
+  r <- readProcess "./tlsfSynt.sh" ltlCommandArgs []
+  return r
+
+
+prFormulae 
+  :: S.Configuration -> S.Specification -> String
+
+prFormulae c s = case S.apply c s of
+  Left err     -> show err
+  Right formulae -> formulae
+
+-----------------------------------------------------------------------------
+
+-- | Prints the input signals of the given specification.
+
+prInputs
+  :: S.Configuration -> S.Specification -> String
+
+prInputs c s = case S.inputs c s of
+  Left err     -> show err
+  Right (x:xr) -> x ++ concatMap ((:) ',' . (:) ' ') xr
+
+-----------------------------------------------------------------------------
+
+-- | Prints the output signals of the given specification.
+
+prOutputs
+  :: S.Configuration -> S.Specification -> String
+
+prOutputs c s = case S.outputs c s of
+  Left err     -> show err
+  Right (x:xr) -> x ++ concatMap ((:) ',' . (:) ' ') xr
+
