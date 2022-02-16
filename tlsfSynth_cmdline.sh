bf31ecbb33ca1d50df2defcbb61cb596a9139f4c
Mark Santolucito <msantolu@barnard.edu>
2022-02-16 12:17:43 -0500

    add a handy tlsf script

--- tlsfSynth_cmdline.sh
+++ tlsfSynth_cmdline.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
+# a script to run ltlsynt on the command line by passing in a .tlsf file
+
 filename=$(basename -- "$1")
 extension="${filename##*.}"
 filename="${filename%.*}"
 LTL=$(syfco $1 -f ltlxba -m fully)
 IN=$(syfco $1 --print-input-signals)
 OUT=$(syfco $1 --print-output-signals)
-ltlsynt --formula="$LTL" --ins="$IN" --outs="$OUT" > $filename.hoa
+ltlsynt --formula="$LTL" --ins="$IN" --outs="$OUT" --hoaf="i" > $filename.hoa
