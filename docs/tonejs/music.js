5bc3e2b69c9a319d43bb3b7111addbafe3f0f881
Danielle Cai <dc3347@columbia.edu>
2022-03-27 13:30:28 -0400

    re-added sonification for 1 note G

---
+++ docs/tonejs/music.js
@@ -0,0 +1,18 @@
+document.getElementById("play-button1").addEventListener("click", function() {
+
+  if (Tone.Transport.state !== 'started') {
+    const synthA = new Tone.Synth().toDestination();
+    //play a note every quarter-note
+    const loopA = new Tone.Loop(time => {
+      synthA.triggerAttackRelease("G4", "8n", time);
+    }, "4n").start(0);
+
+    Tone.Transport.start();
+
+  } else {
+    Tone.Transport.stop(); 
+  }
+
+});
+
+
