87f2f7e49388f5acbe647f073dbe048e0e2574bd
Danielle Cai <dc3347@columbia.edu>
2022-04-26 01:01:30 -0400

    added rhythms tsl and js code

---
+++ docs/tonejs/Rhythm.js
@@ -0,0 +1,44 @@
+var first = true;
+document.getElementById("play-button4").addEventListener("click", function() {
+  if (first) {
+      const synthA = new Tone.Synth().toDestination();
+
+      //play a note every quarter-note
+      const loopA = new Tone.Loop(time => {
+        updateStateMachine();
+	synthA.triggerAttackRelease(noteToPlay, rhythm, time);
+      }, "4n").start(0);
+
+      first = false;
+
+  }
+  if (Tone.Transport.state !== 'started') {
+
+      // the loop starts when the Transport is started
+      Tone.Transport.start()
+
+  } else {
+    Tone.Transport.stop();
+  }
+});
+
+var currentState = 0
+var noteToPlay = "E4"
+var rhythm = "2n"
+function updateStateMachine() {
+   if (currentState ==  0 ){
+        if (true){
+            noteToPlay = "E4";
+            rhythm = "2n";
+            currentState = 1;
+        }
+    }
+    else if (currentState ==  1 ){
+        if (true){
+            noteToPlay = "G4";
+            rhythm = "8n";
+            currentState = 0;
+        }
+    }
+}
+
