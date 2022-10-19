3271e5b9d9f7ccd18896f3f217e4c5f73cef11f7
Mark Santolucito <mark.santolucito@yale.edu>
2022-10-19 11:24:35 -0400

    post process tslmt in code generation

--- src/lib/TSL/Writer/HOA/Python.hs
+++ src/lib/TSL/Writer/HOA/Python.hs
@@ -1,109 +1,95 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE RankNTypes #-}
 ----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+{-# LANGUAGE RecordWildCards #-}
+
 -- |
 -- Module      :  Main
 -- Maintainer  :  Mark Santolucito
 --
 -- Generates python code from a HOA file generated from a TSL spec
---
------------------------------------------------------------------------------
-
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE ImplicitParams        #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE OverloadedStrings #-}
-
-
 module TSL.Writer.HOA.Python
-  ( implementHoa
-  ) where
+  ( implementHoa,
+  )
+where
 
-import TSL.Writer.HOA.Utils
-
-import Data.Tuple ( swap )
-
-import qualified Data.Text as T
-
-import Hanoi
-    ( HOA(..), AcceptanceSet, Label, State )
-import Data.Maybe ( fromJust )
-import Data.List as List (intercalate, sortOn)
-
-
-import qualified Data.Map as M
-import Data.Set as Set (Set, toList)
 import qualified Data.Bifunctor
+import Data.List as List (intercalate, sortOn)
+import qualified Data.Map as M
+import Data.Maybe (fromJust)
+import Data.Set as Set (Set, toList)
+import qualified Data.Text as T
+import Data.Tuple (swap)
 import Finite
+import Hanoi
+  ( AcceptanceSet,
+    HOA (..),
+    Label,
+    State,
+  )
+import TSL.Writer.HOA.Utils
 
 implementHoa :: HOA -> String
 implementHoa = concat . printHOALines
 
 printHOALines :: HOA -> [String]
 printHOALines hoa@HOA {..} =
-  let ?bounds = hoa in
-  let
-    -- TODO pretty sure this doesnt need to be sorted
-    apNamesSorted = map (Data.Bifunctor.first atomicPropositionName) $ sortOn (atomicPropositionName. fst) $ zip values [0..]
-    apNamesMap = M.fromList $ map swap apNamesSorted
+  let ?bounds = hoa
+   in let -- TODO pretty sure this doesnt need to be sorted
+          apNamesSorted = map (Data.Bifunctor.first atomicPropositionName) $ sortOn (atomicPropositionName . fst) $ zip values [0 ..]
+          apNamesMap = M.fromList $ map swap apNamesSorted
 
-    printState :: FiniteBounds HOA => State -> [String]
-    printState s =
-      unwords (
-        ["if (currentState == "]
-        ++
-        [strInd s]
-        ++
-        ["):"]
-      )
-      :
-      map printEdge (toList $ edges s)
+          printState :: FiniteBounds HOA => State -> [String]
+          printState s =
+            unwords
+              ( ["if (currentState == "]
+                  ++ [strInd s]
+                  ++ ["):"]
+              ) :
+            map printEdge (toList $ edges s)
 
-    printEdge ::
-          FiniteBounds HOA
-      => ([State], Maybe Label, Maybe (Set AcceptanceSet))
-      -> String
-    printEdge edge =
-      let
-        (target, label, _) = edge
-        -- we should only ever had one target state in TSL models (I think), so `head` works
-        stateUpdate = "currentState = " ++ strInd (head target)
-      in
-        printLabel (fromJust label) stateUpdate
+          printEdge ::
+            FiniteBounds HOA =>
+            ([State], Maybe Label, Maybe (Set AcceptanceSet)) ->
+            String
+          printEdge edge =
+            let (target, label, _) = edge
+                -- we should only ever had one target state in TSL models (I think), so `head` works
+                stateUpdate = "currentState = " ++ strInd (head target)
+             in printLabel (fromJust label) stateUpdate
 
-    printLabel :: FiniteBounds HOA => Label -> String -> String
-    printLabel label stateUpdate = let
-
-        splitFormulas = formulaToList label
-        termStringList = map (map (printTSLFormula negationOperator (strIndWithMap apNamesMap))) splitFormulas :: [[String]]
-        predUpds = splitPredUpdates negationOperator termStringList
-        predUpdToCode (preds, upds) = let
-            conditional =  if null preds then "True" else intercalate (" "++conjunctionOperator++ indent 3) preds
-            body = indent 4 ++ intercalate (indent 4) (map updateToAssignment upds ++ [stateUpdate])
-          in
-            "if (" ++ conditional ++ "):" ++ body
-      in
-        concatMap (\x -> indent 2 ++ predUpdToCode x) predUpds
-
-  in
-    intercalate ["\nel"] $ map printState values
+          printLabel :: FiniteBounds HOA => Label -> String -> String
+          printLabel label stateUpdate =
+            let splitFormulas = formulaToList label
+                termStringList = map (map (printTSLFormula negationOperator (strIndWithMap apNamesMap))) splitFormulas :: [[String]]
+                predUpds = splitPredUpdates negationOperator termStringList
+                predUpdToCode (preds, upds) =
+                  let conditional = if null preds then "True" else intercalate (" " ++ conjunctionOperator ++ indent 3) preds
+                      body = indent 4 ++ intercalate (indent 4) (map updateToAssignment upds ++ [stateUpdate])
+                   in "if (" ++ conditional ++ "):" ++ body
+             in concatMap (\x -> indent 2 ++ predUpdToCode x) predUpds
+       in intercalate ["\nel"] $ map printState values
 
 -----------------------------------------------------------------------------
--- | Language specific functions
 
+-- | Language specific functions
 updateToAssignment :: String -> String
-updateToAssignment x = let
-  constsSaved = T.unpack $ T.replace "()" "cccccc" $ T.pack x
-  noBrackets = filter (\c -> not $ c `elem` ['[',']','(',')']) constsSaved
-  [val, assignment] = T.splitOn " <- " $ T.pack noBrackets
-  fxnParts = map T.strip $ T.splitOn " " assignment
-  params = if tail fxnParts == []
-           then ""
-           else T.concat ["(", (T.intercalate ", " $ tail fxnParts), ");"] 
- in
-   T.unpack $ T.replace "cccccc" "()" $ T.concat [val, " = ", head fxnParts, params]
-   
+updateToAssignment x =
+  let constsSaved = T.unpack $ T.replace "()" "cccccc" $ T.pack x
+      noBrackets = filter (\c -> not $ c `elem` ['[', ']', '(', ')']) constsSaved
+      [val, assignment] = T.splitOn " <- " $ T.pack noBrackets
+      fxnParts = map (replaceTSLMT . T.strip) $ T.splitOn " " assignment
+      params =
+        if tail fxnParts == []
+          then ""
+          else T.concat ["(", (T.intercalate ", " $ tail fxnParts), ");"]
+   in T.unpack $ T.replace "cccccc" "()" $ T.concat [val, " = ", head fxnParts, params]
+
 negationOperator :: String
 negationOperator = "not"
 
--- src/lib/TSL/Writer/HOA/Utils.hs
+++ src/lib/TSL/Writer/HOA/Utils.hs
@@ -1,22 +1,27 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 
 module TSL.Writer.HOA.Utils
-  ( brRound
-  , indent
-  , strInd
-  , strIndWithMap
-  , translateToTSL
-  , formulaToList
-  , splitPredUpdates
-  , printTSLFormula
-  ) where
-import Finite ( v2t, Finite(offset, index), FiniteBounds )
-import Hanoi (HOA, Formula (FAnd, FOr, FVar, FNot, FTrue, FFalse))
+  ( brRound,
+    indent,
+    strInd,
+    strIndWithMap,
+    translateToTSL,
+    formulaToList,
+    splitPredUpdates,
+    printTSLFormula,
+    replaceTSLMT,
+  )
+where
+
+import Data.List (isInfixOf, isPrefixOf)
 import qualified Data.Map as M
+import qualified Data.Text as Text
+import Finite (Finite (index, offset), FiniteBounds, v2t)
+import Hanoi (Formula (FAnd, FFalse, FNot, FOr, FTrue, FVar), HOA)
 import qualified TSL.Logic as T
-import Data.List ( isPrefixOf, isInfixOf )
 
 wrap :: String -> String -> String -> String
 wrap prefix suffix s = prefix ++ s ++ suffix
@@ -25,58 +30,63 @@ brRound :: String -> String
 brRound = wrap "(" ")"
 
 indent :: Int -> String
-indent n = let
-    indentLevel = "  "
-  in
-    "\n" ++ concat (replicate n indentLevel)
-  
+indent n =
+  let indentLevel = "  "
+   in "\n" ++ concat (replicate n indentLevel)
 
 strInd :: (Finite HOA a, FiniteBounds HOA) => a -> String
 strInd a = show (index a - offset (v2t a))
-  
+
 strIndWithMap :: (Finite HOA a, FiniteBounds HOA) => M.Map Int String -> a -> String
 strIndWithMap nameMap a = translateToTSL $ nameMap M.! (index a - offset (v2t a))
 
 translateToTSL :: String -> String
 translateToTSL t =
   if "p0" `isPrefixOf` t
-  then generateTSLString T.Check T.decodeInputAP t
-  else generateTSLString (uncurry T.Update) T.decodeOutputAP t
+    then generateTSLString T.Check T.decodeInputAP t
+    else generateTSLString (uncurry T.Update) T.decodeOutputAP t
 
 generateTSLString :: forall a b. Show a => (b -> T.Formula String) -> (String -> Either a b) -> String -> String
 generateTSLString tslType decoder x =
-  either show (T.tslFormula id. tslType) $
+  either show (T.tslFormula id . tslType) $
     decoder x
 
-    
--- | Given a list of 
+-- | replace eg real3.4() with 3.4 and
+--   int4() with 4
+replaceTSLMT :: Text.Text -> Text.Text
+replaceTSLMT x =
+  let r typeName w =
+        if typeName `Text.isPrefixOf` w
+          then Text.drop (Text.length typeName) $ Text.take (Text.length w - 6) w
+          else w
+   in r "real" $ r "int" x
+
+-- | Given a list of
 splitPredUpdates :: String -> [[String]] -> [([String], [String])]
-splitPredUpdates negationOperator terms = let
-    sortTerm t (preds, upds) =
-      if "<-" `isInfixOf` t
-      then if negationOperator `isPrefixOf` t
-        then (preds, upds)
-        else (preds, t:upds)
-      else (t:preds, upds)
-  in
-    map (foldr sortTerm ([],[])) terms
+splitPredUpdates negationOperator terms =
+  let sortTerm t (preds, upds) =
+        if "<-" `isInfixOf` t
+          then
+            if negationOperator `isPrefixOf` t
+              then (preds, upds)
+              else (preds, t : upds)
+          else (t : preds, upds)
+   in map (foldr sortTerm ([], [])) terms
 
 -- | in TSL, the formulas on the edges will always be conjunctions separated by disjunctions
---   so we just pull the formula apart, then can sort by preds and updates 
+--   so we just pull the formula apart, then can sort by preds and updates
 formulaToList :: Formula a -> [[Formula a]]
 formulaToList f =
-  let
-    conjunctionsToList :: Formula a -> [Formula a]
-    conjunctionsToList f = case f of
-      FAnd fs -> concatMap conjunctionsToList fs
-      _ -> [f]
+  let conjunctionsToList :: Formula a -> [Formula a]
+      conjunctionsToList f = case f of
+        FAnd fs -> concatMap conjunctionsToList fs
+        _ -> [f]
 
-    disjunctionsToList :: Formula a -> [Formula a]
-    disjunctionsToList f = case f of
-      FOr fs -> concatMap disjunctionsToList fs
-      _ -> [f]
-  in
-    map conjunctionsToList $ disjunctionsToList f
+      disjunctionsToList :: Formula a -> [Formula a]
+      disjunctionsToList f = case f of
+        FOr fs -> concatMap disjunctionsToList fs
+        _ -> [f]
+   in map conjunctionsToList $ disjunctionsToList f
 
 -- | When printing a TSL formula, we should only have the vars or negated vars
 --   if we have anything else, we did not have only conjunctions and need to revisit assumption of formulaToList
@@ -87,6 +97,5 @@ printTSLFormula negationOperator showVar = \case
   FTrue -> "True"
   FFalse -> "False"
   x -> error $ "unexpected formula structure on transition: " ++ show x
-
   where
     printSubFormula = brRound . printTSLFormula negationOperator showVar
