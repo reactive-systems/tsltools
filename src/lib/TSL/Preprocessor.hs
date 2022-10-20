455fdd6c29f4525c41b3b9a0d0ceecfd00b9a66d
Mark Santolucito <mark.santolucito@yale.edu>
2022-10-19 22:34:32 -0400

    add support for -> operator in tslmt

--- src/lib/TSL/Preprocessor.hs
+++ src/lib/TSL/Preprocessor.hs
@@ -1,104 +1,99 @@
+{-# LANGUAGE FlexibleContexts #-}
 ------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+-------------------------------------------------------------------------------
+
 -- |
 -- Module      :  TSL.ModuloTheories.Preprocessor
--- Description :  
+-- Description :
 -- Maintainer  :  Wonhyuk Choi
-
--------------------------------------------------------------------------------
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleContexts #-}
-
--------------------------------------------------------------------------------
-
-module TSL.Preprocessor(preprocess) where
+module TSL.Preprocessor (preprocess) where
 
 -------------------------------------------------------------------------------
 
 import Control.Applicative ((<|>))
-
-import Text.Parsec (ParseError, endBy, sepBy, try, eof)
-
-import Text.Parsec.Combinator (chainl1)
-
-import Text.Parsec.String (Parser)
-
 import TSL.Error (Error, parseError)
-
+import Text.Parsec (ParseError, endBy, eof, sepBy, try)
 import qualified Text.Parsec as Parsec
+import Text.Parsec.Combinator (chainl1)
+import Text.Parsec.String (Parser)
 
 -------------------------------------------------------------------------------
 
-data Op = 
-      Eq
-    | Lt
-    | Gt
-    | Lte
-    | Gte
-    | Add
-    | Sub
-    | Mult
-    | Div
+data Op
+  = Eq
+  | Lt
+  | Gt
+  | Lte
+  | Gte
+  | Add
+  | Sub
+  | Mult
+  | Div
 
 instance Show Op where
   show = \case
-    Eq   -> "eq"
-    Lt   -> "lt"
-    Gt   -> "gt"
-    Lte  -> "lte"
-    Gte  -> "gte"
-    Add  -> "add"
-    Sub  -> "sub"
+    Eq -> "eq"
+    Lt -> "lt"
+    Gt -> "gt"
+    Lte -> "lte"
+    Gte -> "gte"
+    Add -> "add"
+    Sub -> "sub"
     Mult -> "mult"
-    Div  -> "div"
+    Div -> "div"
 
 ltOp :: Parser Op
 ltOp = do
-    _ <- Parsec.string "<"
-    _ <- Parsec.notFollowedBy $ Parsec.oneOf "-="
-    return Lt
+  _ <- Parsec.string "<"
+  _ <- Parsec.notFollowedBy $ Parsec.oneOf "-="
+  return Lt
 
 lteOp :: Parser Op
 lteOp = do
-    _ <- Parsec.string "<="
-    return Lte
+  _ <- Parsec.string "<="
+  return Lte
 
 gtOp :: Parser Op
 gtOp = do
-    _ <- Parsec.string ">"
-    _ <- Parsec.notFollowedBy $ Parsec.char '='
-    return Gt
+  _ <- Parsec.string ">"
+  _ <- Parsec.notFollowedBy $ Parsec.char '='
+  return Gt
 
 gteOp :: Parser Op
 gteOp = do
-    _ <- Parsec.string "<="
-    return Gte
+  _ <- Parsec.string "<="
+  return Gte
 
 eqOp :: Parser Op
 eqOp = do
-    _ <- Parsec.string "="
-    return Eq
+  _ <- Parsec.string "="
+  return Eq
 
 addOp :: Parser Op
 addOp = do
-    _ <- Parsec.string "+"
-    return Add
+  _ <- Parsec.string "+"
+  return Add
 
 subOp :: Parser Op
 subOp = do
-    _ <- Parsec.string "-"
-    return Sub
+  _ <- Parsec.string "-"
+  _ <- Parsec.notFollowedBy $ Parsec.char '>'
+  return Sub
 
 multOp :: Parser Op
 multOp = do
-    _ <- Parsec.string "*"
-    return Mult
+  _ <- Parsec.string "*"
+  return Mult
 
 divOp :: Parser Op
 divOp = do
-    _ <- Parsec.string "/"
-    return Mult
+  _ <- Parsec.string "/"
+  return Mult
 
 comparator :: Parser Op
 comparator = ltOp <|> gtOp <|> lteOp <|> gteOp <|> eqOp
@@ -111,41 +106,41 @@ binOp = comparator <|> arithmetic
 
 -------------------------------------------------------------------------------
 
-data Value =
-      TSLInt  Int
-    | TSLReal Float
-    | Symbol String
-    | SymbolList [Value]
-    | BinOp Op Value Value
-    | Paren Value
+data Value
+  = TSLInt Int
+  | TSLReal Float
+  | Symbol String
+  | SymbolList [Value]
+  | BinOp Op Value Value
+  | Paren Value
 
 instance Show Value where
   show = \case
-    TSLInt i         -> "int"  ++ show i ++ "()"
-    TSLReal r        -> "real" ++ show r ++ "()"
-    Symbol s         -> s
+    TSLInt i -> "int" ++ show i ++ "()"
+    TSLReal r -> "real" ++ show r ++ "()"
+    Symbol s -> s
     SymbolList slist -> unwords $ map show slist
     BinOp op lhs rhs -> unwords $ [show op, show lhs, show rhs]
-    Paren v          -> "(" ++ show v ++ ")"
+    Paren v -> "(" ++ show v ++ ")"
 
 int :: Parser Value
 int = do
-    int <- Parsec.many1 Parsec.digit
-    return $ TSLInt $ read int
+  int <- Parsec.many1 Parsec.digit
+  return $ TSLInt $ read int
 
 real :: Parser Value
 real = do
-    beforePoint <- Parsec.many1 Parsec.digit
-    _ <- Parsec.char '.'
-    afterPoint <- Parsec.many1 Parsec.digit
-    return $ TSLReal $ read $ beforePoint ++ ('.':afterPoint)
+  beforePoint <- Parsec.many1 Parsec.digit
+  _ <- Parsec.char '.'
+  afterPoint <- Parsec.many1 Parsec.digit
+  return $ TSLReal $ read $ beforePoint ++ ('.' : afterPoint)
 
 binOpTerm :: Parser (Value -> Value -> Value)
 binOpTerm = do
-    _ <- maybeSpaces
-    op <- binOp
-    _ <- maybeSpaces
-    return $ BinOp op
+  _ <- maybeSpaces
+  op <- binOp
+  _ <- maybeSpaces
+  return $ BinOp op
 
 parens :: Parser Value
 parens = do
@@ -158,8 +153,8 @@ parens = do
 
 symbol :: Parser Value
 symbol = do
-    str <- Parsec.many1 $ Parsec.noneOf "() \n\r"
-    return $ Symbol str
+  str <- Parsec.many1 $ Parsec.noneOf "() \n\r"
+  return $ Symbol str
 
 atom :: Parser Value
 atom = try real <|> int <|> symbol
@@ -175,24 +170,27 @@ tslValue = chainl1 (factor <* maybeSpaces) (try binOpTerm)
 
 tslSequence :: Parser Value
 tslSequence = SymbolList <$> (tslValue >>= recursion)
-  where recursion this = do { _    <- maybeSpaces
-                            ; next <- tslValue
-                            ; rest <- recursion next
-                            ; return $ this:rest
-                            }
-                           <|> return [this]
+  where
+    recursion this =
+      do
+        _ <- maybeSpaces
+        next <- tslValue
+        rest <- recursion next
+        return $ this : rest
+        <|> return [this]
 
 lineParser :: Parser [Value]
 lineParser = maybeSpaces *> sepBy tslSequence maybeSpaces
-    
+
 fileParser :: Parser [[Value]]
 fileParser = endBy lineParser Parsec.endOfLine
 
 parse :: String -> Either ParseError [[Value]]
 parse input = Parsec.parse (fileParser <* eof) err input
-  where err = "Parser Failed!:\n>>>>>\n" ++ input ++ "\n<<<<<\n"
+  where
+    err = "Parser Failed!:\n>>>>>\n" ++ input ++ "\n<<<<<\n"
 
 preprocess :: String -> Either Error String
 preprocess s = case parse s of
   Right values -> return $ unlines $ map (unwords . map show) values
-  Left err     -> parseError err
+  Left err -> parseError err
